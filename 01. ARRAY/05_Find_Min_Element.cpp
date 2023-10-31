7265564b599b1ff898e5805d84a9fdaffd71222e
ekhlakhahmad <ekhlakhahmad95@gmail.com>
2023-10-31 23:28:47 +0530

    Added some programs

---
+++ 01. ARRAY/05_Find_Min_Element.cpp
@@ -0,0 +1,16 @@
+// Write a program to Given an array of integers, find the Minimum element.
+
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[10] = {2, 23, 12, 4, 5, 9, 8, 6, 52, 15};
+
+    int maxElement = arr[0];
+    for (int i = 0; i < 10; i++){
+        if(maxElement > arr[i]){
+            maxElement = arr[i];
+        }
+    }
+    cout << "Max Element is " << maxElement;
+    return 0;
+}
--- 01. ARRAY/tempCodeRunnerFile.cpp
+++ 01. ARRAY/tempCodeRunnerFile.cpp
@@ -1,3 +1,30 @@
-else{
-        //     cout << -1;
-        // }
+
+// Question 1:
+// Given an array of integers, find the maximum element.
+
+// Question 2:
+// Given an array of integers, find the minimum element.
+
+// Question 3:
+// Given an array of integers, find the sum of all even numbers in the array.
+
+// Question 4:
+// Given an array of integers, find the product of all elements.
+
+// Question 5:
+// Given an array of integers, find the average of all elements.
+
+// Question 6:
+// Given an array of integers, find the count of elements that are greater than a specified value X.
+
+// Question 7:
+// Given an array of integers, remove all duplicate elements and return a new array with unique elements.
+
+// Question 8:
+// Given an array of integers, reverse the order of elements in the array.
+
+// Question 9:
+// Given two arrays, find the common elements between them.
+
+// Question 10:
+// Given an array of integers, find the second largest element in the array.
